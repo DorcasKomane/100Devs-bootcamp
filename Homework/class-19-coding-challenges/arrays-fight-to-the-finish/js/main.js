64413924850d04c80574f3d3762c5219cc0e9cd6
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-04-25 11:23:52 +0200

    Added solution to coding challenge #1 from class 19.

--- Homework/class-19-coding-challenges/arrays-fight-to-the-finish/js/main.js
+++ Homework/class-19-coding-challenges/arrays-fight-to-the-finish/js/main.js
@@ -1,7 +1,13 @@
-//Create an array of movie titles. Loop through the array and each element to the h2.
+//Create an array of movie titles. Loop through the array and add each element to the h2.
+const movies = ['The Devil Wears Prada', 'Spiderman', 'Redemption'];
 
+for(let i = 0; i < movies.length; i++){
+    document.querySelector('h2').innerText += movies[i]; 
+}
 
-//Create an array of numbers. Loop through the array and three to each number and replace the old number.
+//Create an array of numbers. Loop through the array and add three to each number and replace the old number.
+let numbers = [1, 2, 3, 4, 5];
+
 
 
 //Find the average of all the numbers from question three
