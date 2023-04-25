637f3bbdf9de0de4aa98e84a6246e12a51cb6bb3
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-04-25 11:42:48 +0200

    Added solution to coding challenge #2 from class 19.

--- Homework/class-19-coding-challenges/arrays-fight-to-the-finish/js/main.js
+++ Homework/class-19-coding-challenges/arrays-fight-to-the-finish/js/main.js
@@ -7,7 +7,16 @@ for(let i = 0; i < movies.length; i++){
 
 //Create an array of numbers. Loop through the array and add three to each number and replace the old number.
 let numbers = [1, 2, 3, 4, 5];
+let newArray = [];
 
+// for(let i = 0; i < numbers.length; i++){
+//     newArray.push(numbers[i] + 3);
+// }
+// console.log(newArray);
 
+numbers.forEach((item)=>{
+    newArray.push(item + 3)
+});
+console.log(newArray);
 
 //Find the average of all the numbers from question three
