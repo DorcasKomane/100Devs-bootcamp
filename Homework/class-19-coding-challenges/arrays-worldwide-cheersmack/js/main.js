c4da92f5ca011cc58b8b5a7ee0579237cd54c775
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-04-25 18:29:56 +0200

    Added solution to coding challenge in the arrays-worldwide-cheersmack folder from class 19.

--- Homework/class-19-coding-challenges/arrays-worldwide-cheersmack/js/main.js
+++ Homework/class-19-coding-challenges/arrays-worldwide-cheersmack/js/main.js
@@ -1,1 +1,13 @@
-//Create a function that takes in a number. The function should return an array that contains every number from 1 to that number as seperate elements
+//Create a function that takes in a number. 
+//The function should return an array that contains every number 
+//from 1 to that number as seperate elements
+function numbersFrom1toX(num){
+    let arr = [];
+
+    for(let i = 1; i <= num; i++){
+        arr.push(i);
+    }
+    return arr;
+}
+
+console.log(numbersFrom1toX(10));
