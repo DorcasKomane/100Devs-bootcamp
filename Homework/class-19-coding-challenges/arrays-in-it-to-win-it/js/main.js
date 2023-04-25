e4d4ecd923db980e6e6eb5bc5ea245a36b9de390
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-04-25 18:21:04 +0200

    Added solutions to coding challenges in the arrays-in-it-to-win-it folder from class 19.

--- Homework/class-19-coding-challenges/arrays-in-it-to-win-it/js/main.js
+++ Homework/class-19-coding-challenges/arrays-in-it-to-win-it/js/main.js
@@ -1,7 +1,12 @@
 //Create an array of movies with at least three movies.
+const movies = ['Independence Day', 'Starwars', 'The Godfather'];
 
 //Using the array from above, store the first movie in a variable
+let firstMovie = movies[0];
 
 //Get the length of the original array and store it in a new variable
+let moviesLength = movies.length;
 
-//Get the last element in that array and store it in a new variable. What if your array was really large and you didn't know the last index? Would your solution still work?
+//Get the last element in that array and store it in a new variable. 
+//What if your array was really large and you didn't know the last index? Would your solution still work?
+let lastElem = movies[movies.length - 1];
