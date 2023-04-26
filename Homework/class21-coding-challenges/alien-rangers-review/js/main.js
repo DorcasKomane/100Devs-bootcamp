9a5e72ad82f2e0c973831eef575bd968cc02b732
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-04-26 12:13:02 +0200

    Added solution to the coding challenge in the folder alien-rangers-review folder from class 21.

--- Homework/class21-coding-challenges/alien-rangers-review/js/main.js
+++ Homework/class21-coding-challenges/alien-rangers-review/js/main.js
@@ -1,9 +1,33 @@
 //Arrays
 
-//Create and array of tv shows. Loop through and print each show to the console
+//Create an array of tv shows. Loop through and print each show to the console
+let movies = ['Hidden Figures', 'When They See Us', 'The Devil Wears Prada'];
 
-//Create and array of numbers
+for(let i = 0; i < movies.length; i++){
+    console.log(movies[i]);
+}
+
+//Create an array of numbers
 //Return a new array of numbers that includes every even number from the previous Arrays
+let arr = [1, 2, 3, 4, 5];
+let newArr = [];
+
+for(let i = 0; i < arr.length; i++){
+    if(arr[i] % 2 === 0){
+        newArr.push(arr[i]);
+    }
+}
+
+console.log(newArr);
 
 //Create a function that takes in an array of numbers
 //Alert the sum of the second lowest and the second highest number
+function lowestAndHighest(arr){
+    arr.sort((a, b) => a - b);
+    
+    let sum = arr[1] + arr[arr.length - 2];
+
+    alert(sum);
+}
+
+lowestAndHighest([4, 2, 7, 5, 3, 19]);
