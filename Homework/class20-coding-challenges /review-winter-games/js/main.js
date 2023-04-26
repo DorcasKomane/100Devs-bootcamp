225e4b7b28b4a8d25062b76f357a7bdd017eb38f
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-04-26 11:43:45 +0200

    Added solution to coding challenge from the review-winter-games folder from class 20.

--- Homework/class20-coding-challenges /review-winter-games/js/main.js
+++ Homework/class20-coding-challenges /review-winter-games/js/main.js
@@ -1,1 +1,15 @@
-//Create a function that takes in an array of numbers. Return a new array containing every even number from the original array (do not use map or filter)
+//Create a function that takes in an array of numbers. Return a new array 
+//containing every even number from the original array (do not use map or filter)
+function returnEvenArray(arr){
+    let evenArr = [];
+
+    for(let i = 0; i < arr.length; i++){
+        if(arr[i] % 2 === 0){
+            evenArr.push(arr[i]);
+        }
+    }
+
+    return evenArr;
+}
+
+console.log(returnEvenArray([1, 2, 3, 4, 5, 6]));
