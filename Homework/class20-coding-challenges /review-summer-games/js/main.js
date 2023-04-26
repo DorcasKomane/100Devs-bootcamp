c98f67fcd15de5e0ce961426deeef01d69e630b4
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-04-26 11:34:00 +0200

    Added solution to the coding challenge in the review-summer-games folder from class 20.

--- Homework/class20-coding-challenges /review-summer-games/js/main.js
+++ Homework/class20-coding-challenges /review-summer-games/js/main.js
@@ -1,1 +1,12 @@
 //Create a function that takes in an array of numbers. Multiply each number together and alert the product. 
+function multiplyArrayElements(arr){
+    let product = 1;
+
+    for(let i = 0; i < arr.length; i++){
+        product *= arr[i];
+    }
+
+    alert(product);
+}
+
+multiplyArrayElements([1, 2, 3, 4]);
