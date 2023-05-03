6975b4491490fdf8ee6267c285d62400f9cd9324
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-05-03 16:45:04 +0200

    Added solution to thejsway.net coding challenge #2 (Number Comparison) from [Add Conditions] section.

--- Homework/thejsway-coding-challenges/thejsway.js
+++ Homework/thejsway-coding-challenges/thejsway.js
@@ -26,3 +26,14 @@ switch(dayName){
     default:
         console.log('Not a valid day name.');
 }
+
+// Program: Number Comparison
+let num1 = Number(prompt('Enter first number'));
+let num2 = Number(prompt('Enter second number'));
+
+if(num1 > num2){
+    console.log(`${num1} is greater than ${num2}`);
+}else if(num1 < num2){
+    console.log(`${num1} is less than ${num2}`);
+}else 
+    console.log(`${num1} is equal to ${num2}`);
