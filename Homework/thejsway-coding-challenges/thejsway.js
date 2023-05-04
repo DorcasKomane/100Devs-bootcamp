076aed1fb79d2d4881f21ddb29896ae320fef3f9
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-05-04 12:26:53 +0200

    Added solution to challenge #3 (Input Validation) from the [Repeat Statements] section.

--- Homework/thejsway-coding-challenges/thejsway.js
+++ Homework/thejsway-coding-challenges/thejsway.js
@@ -108,21 +108,50 @@
 
 //Program: Parity
 
+// //Version 1
+// for (let i = 1; i <= 10; i++) {
+//     if (i % 2 === 0) {
+//       console.log(`${i} is even`);
+//     }else{
+//         console.log(`${i} is odd`);
+//     }
+//  }
+
+// //Version 2
+// let start = Number(prompt('Enter the initial number:'));
+// for (let i = start; i < (start + 10); i++) {
+//     if (i % 2 === 0) {
+//       console.log(`${i} is even`);
+//     }else{
+//         console.log(`${i} is odd`);
+//     }
+// }
+
+//Version 3 using While Loop
+// let start = Number(prompt('Enter the initial number:'));
+// let i = start;
+
+// while(i < (start + 10)){
+//     if (i % 2 === 0) {
+//         console.log(`${i} is even`);
+//     }else{
+//         console.log(`${i} is odd`);
+//     }
+//     i++
+// }
+
+//Program: Input Validation
+
 //Version 1
-for (let i = 1; i <= 10; i++) {
-    if (i % 2 === 0) {
-      console.log(`${i} is even`);
-    }else{
-        console.log(`${i} is odd`);
-    }
- }
+let number = '';
+
+while(number <= 100){
+    number = Number(prompt('Enter number:'));
+}
 
 //Version 2
-let start = Number(prompt('Enter the initial number:'));
-for (let i = start; i < (start + 10); i++) {
-    if (i % 2 === 0) {
-      console.log(`${i} is even`);
-    }else{
-        console.log(`${i} is odd`);
-    }
+//let number = '';
+
+while(number < 50 || number > 100){
+    number = Number(prompt('Enter number:'));
 }
