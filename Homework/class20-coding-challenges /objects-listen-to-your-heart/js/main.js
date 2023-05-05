dd1e48c5e65a6dbcf793ab9e07d4f4802307f25b
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-05-05 11:09:30 +0200

    Added solution to the stopwatch object coding challenge in the objects-listen-to-your-heart folder.

--- Homework/class20-coding-challenges /objects-listen-to-your-heart/js/main.js
+++ Homework/class20-coding-challenges /objects-listen-to-your-heart/js/main.js
@@ -1,1 +1,18 @@
 //Create a stopwatch object that has four properties and three methods
+let stopwatch = {};
+
+stopwatch.color = 'black';
+stopwatch.size = 'small';
+stopwatch.shape = 'square';
+
+stopwatch.start = function start(){
+    console.log('Starting...');
+}
+
+stopwatch.stop = function stop(){
+    console.log('Stopped');
+}
+
+stopwatch.alarm = function alarm(){
+    console.log('Ping! Ping! Ping!');
+}
