8511d3edc0b0a89fad82b3ba5b1738cee557d190
DorcasKomane <79631289+DorcasKomane@users.noreply.github.com>
2023-05-05 11:26:44 +0200

    Added solution to StreetFighter constructor in the objects-street-fighter folder.

--- Homework/class20-coding-challenges /objects-street-fighter/js/main.js
+++ Homework/class20-coding-challenges /objects-street-fighter/js/main.js
@@ -1,1 +1,20 @@
 //Create a street fighter constructor that makes fighting game characters with 4 properties and 3 methods
+
+function StreetFighter(name, gender, height, weapon){
+    this.name = name;
+    this.gender = gender;
+    this.height = height;
+    this.weapon = weapon;
+
+    this.kick = function kick(){
+        console.log('Tiaaaaaaa!');
+    }
+
+    this.scream = function scream(){
+        console.log('Woooooop!');
+    }
+
+    this.transfigure = function transfigure(){
+        console.log('Changing into battle gear...');
+    }
+}
